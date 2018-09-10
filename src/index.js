07da98d9a03f07cd9f3fa2d00d9f1b2b1b935239
kirillstarlight <kirillstarlight@gmail.com>
2018-09-10 22:47:58 +0300

    pass

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  countLoverPare = 0;
+  arrLover = [];
+  for (var i = 0; i < preferences.length; i++) {
+    loverB = parseInt(preferences[i] - 1);
+    loverC = parseInt(preferences[loverB] - 1);
+    loverX = parseInt(preferences[loverC] - 1);
+    if (loverB != i && loverC != i && loverX == i && arrLover.indexOf(i) === -1) {
+      arrLover.push(i);
+      arrLover.push(loverB);
+      arrLover.push(loverC);
+      countLoverPare++;
+    }
+  }
+  return countLoverPare;
 };
